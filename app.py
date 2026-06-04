7d240c46ee50b0d0988486d5d10d972f4aee4335
Aditya Sharma <adityasharma96655@gmail.com>
2026-06-04 14:37:15 +0530

    Replace cors with CORS for Flask app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from config.extension import *
 from config.settings import BaseConfig
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
@@ -9,7 +10,8 @@ db.init_app(app)
 migrate.init_app(app, db)
 jwt.init_app(app)
 # socketio.init_app(app)
-cors.init_app(app)
+# cors.init_app(app)
+CORS(app)
 limiter.init_app(app)
 
 from routes.adminRoutes import adminBP
